e400b10491ea7894163474f3ef2f67ab77d924ae
Kuzzzz <anleial666@163.com>
2019-02-09 03:15:12 +0800

    html update

--- anlei/views.py
+++ anlei/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+def index(request):
+    name = '尊敬的各位领导'
+    return render(request, 'index.html', {'name':name})
--- biyesheji/urls.py
+++ biyesheji/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from anlei import views as al_views
 
 urlpatterns = [
+    url(r'^$', al_views.index),
     path('admin/', admin.site.urls),
 ]
